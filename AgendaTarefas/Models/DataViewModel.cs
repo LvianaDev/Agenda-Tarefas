e437c1baf89510d32f21b30ea4ead6d24838a13f
Lucas Viana <lucasg.viana@outlook.com>
2021-06-09 02:57:17 -0300

    Implementacao Final - Pŕojeto

---
+++ AgendaTarefas/Models/DataViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendaTarefas.Models
+{
+    public class DataViewModel
+    {
+        public string Datas { get; set; }
+
+        public string Identificadores { get; set; }
+    }
+}
